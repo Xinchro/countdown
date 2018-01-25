750e770e49f125869c74a1b4650a2fa15e1e3a5c
Xinchro <TheXrocks@gmail.com>
2018-01-25 16:55:42 +0000

    added port fallback

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-app.listen(process.env.PORT)
+app.listen(process.env.PORT || 8080)
 app.use(express.static(path.join(__dirname, 'public')))
 
 module.exports.app = app
