439285fe3df002273172e67c1cd60d220ff8de40
Xinchro <TheXrocks@gmail.com>
2018-01-25 14:59:04 +0000

    added dotenv

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-app.listen(80)
+app.listen(process.env.PORT)
 app.use(express.static(path.join(__dirname, 'public')))
 
 module.exports.app = app
